15e72244e839ae5137979f3c8683b3d4e9df7cb1
Fraser Davidson <fraser@frontierdigital.net>
2023-07-27 16:50:14 +0100

    WIP [major]

---
+++ config/dev/main.tfvars
@@ -0,0 +1,7 @@
+clusters = [
+  {
+    name               = "foo"
+    kubernetes_version = "1.24.10"
+  }
+]
+rancher_server_key_vault_id = "/subscriptions/7fc96c6f-e857-497a-a993-847b18ff22a2/resourceGroups/demo-dev-uks-rancherserver-rg/providers/Microsoft.KeyVault/vaults/demo-dev-uks-ranserv-kv"
---
+++ src/terraform/variables.tf
@@ -0,0 +1,48 @@
+variable "cluster_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "kubernetes_version" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "rancher_server_key_vault_id" {
+  type = string
+}
+
+variable "set" {
+  type = string
+}
+
+variable "short_location" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "workload_name" {
+  type = string
+}
+
+variable "workload_type" {
+  type = string
+}
+
+variable "workload_version" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
